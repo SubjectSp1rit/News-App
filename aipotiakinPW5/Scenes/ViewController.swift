5216000966e1e08d0e97e49e78b6ad0f47f2bf04
Arseniy <arseniy@MacBook-Air-Arseniy.local>
2024-12-11 18:56:24 +0300

    PW5: Project Initialized

---
+++ aipotiakinPW5/Scenes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aipotiakinPW5
+//
+//  Created by Arseniy on 11.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
