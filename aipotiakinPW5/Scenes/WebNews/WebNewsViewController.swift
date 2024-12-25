c2eb9eac4959a3e1bb2cecbf38d2a7d5bc19fea7
Arseniy <arseniy@MacBook-Air-Arseniy.local>
2024-12-25 18:07:39 +0300

    PW5: Добавлена кнопка для закрытия WebView

--- aipotiakinPW5/Scenes/WebNews/WebNewsViewController.swift
+++ aipotiakinPW5/Scenes/WebNews/WebNewsViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 import WebKit
 
 final class WebNewsViewController: UIViewController {
+    // MARK: - Constants
+    private enum Constants {
+        // closeButton
+        static let closeButtonTitle: String = "Close"
+        static let closeButtonBgColor: UIColor = .clear
+        static let closeButtonTintColor: UIColor = .systemBlue
+        static let closeButtonLeadingIndent: CGFloat = 10
+        static let closeButtonTopIndent: CGFloat = 5
+        static let closeButtonHeight: CGFloat = 30
+        
+        // webView
+        static let webViewTopIndent: CGFloat = 5
+    }
+    
+    private let closeButton: UIButton = UIButton(type: .system)
+    
     // MARK: - Variables
     private var interactor: (WebNewsBusinessLogic & WebNewsDataStore)
     private var webView: WKWebView!
@@ -27,20 +43,56 @@ final class WebNewsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureBackground()
+        configureCloseButton()
         configureWebView()
         loadURL()
     }
     
     // MARK: - Private Methods
+    private func configureBackground() {
+        // Размытие заднего фона
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        view.addSubview(blurEffectView)
+        
+        blurEffectView.pin(to: view)
+    }
+    
+    private func configureCloseButton() {
+        view.addSubview(closeButton)
+        
+        closeButton.backgroundColor = Constants.closeButtonBgColor
+        closeButton.tintColor = Constants.closeButtonTintColor
+        closeButton.setTitle(Constants.closeButtonTitle, for: .normal)
+        
+        closeButton.pinLeft(to: view.leadingAnchor, Constants.closeButtonLeadingIndent)
+        closeButton.pinTop(to: view.topAnchor, Constants.closeButtonTopIndent)
+        closeButton.setHeight(Constants.closeButtonHeight)
+        
+        closeButton.addTarget(self, action: #selector(closeButtonPressed), for: .touchUpInside)
+    }
+    
     private func configureWebView() {
-        webView = WKWebView(frame: view.bounds)
+        webView = WKWebView()
         webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view.addSubview(webView)
+        
+        webView.pinRight(to: view.trailingAnchor)
+        webView.pinLeft(to: view.leadingAnchor)
+        webView.pinTop(to: closeButton.bottomAnchor, Constants.webViewTopIndent)
+        webView.pinBottom(to: view.bottomAnchor)
     }
     
     private func loadURL() {
         let url = interactor.url
         let request = URLRequest(url: url)
         webView.load(request)
+    }
+    
+    // MARK: - Actions
+    @objc
+    private func closeButtonPressed() {
+        dismiss(animated: true)
     }
 }
